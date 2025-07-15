9e711653b3f0d2e1b39d3ba9e3f7eda0844d80e7
Ariel dos Reis Marinho <arieldreis@gmail.com>
2025-07-15 12:17:45 -0300

    New photos

--- SpotifyHash/my_spotify/src/Artist.jsx
+++ SpotifyHash/my_spotify/src/Artist.jsx
@@ -1,10 +1,52 @@
-import React from 'react'
-
+import React from 'react';
+import theWeekend from './assets/Singers/theWeekend.jpg';
+import Artist from './assets/artist.js';
 const Artist = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+        <div className="container-artistas">
+          <div className='artistas'>
+            <img src={theWeekend} alt="Imagem do cantor the Weekend" />
+            <p>The Weeknd</p>
+            <p>Artista</p>
+          </div>
+          <div className='artistas'>
+            <img src={theWeekend} alt="Imagem do cantor the Weekend" />
+            <p>The Weeknd</p>
+            <p>Artista</p>
+          </div>
+          <div className='artistas'>
+            <img src={theWeekend} alt="Imagem do cantor the Weekend" />
+            <p>The Weeknd</p>
+            <p>Artista</p>
+          </div>
+          <div className='artistas'>
+            <img src={theWeekend} alt="Imagem do cantor the Weekend" />
+            <p>The Weeknd</p>
+            <p>Artista</p>
+          </div>
+          <div className='artistas'>
+            <img src={theWeekend} alt="Imagem do cantor the Weekend" />
+            <p>The Weeknd</p>
+            <p>Artista</p>
+          </div>
+          <div className='artistas'>
+            <img src={theWeekend} alt="Imagem do cantor the Weekend" />
+            <p>The Weeknd</p>
+            <p>Artista</p>
+          </div>
+          <div className='artistas'>
+            <img src={theWeekend} alt="Imagem do cantor the Weekend" />
+            <p>The Weeknd</p>
+            <p>Artista</p>
+          </div>
+          <div className='artistas'>
+            <img src={theWeekend} alt="Imagem do cantor the Weekend" />
+            <p>The Weeknd</p>
+            <p>Artista</p>
+          </div>
+        </div>
+    </>
   )
 }
 
--- SpotifyHash/my_spotify/src/Principal.jsx
+++ SpotifyHash/my_spotify/src/Principal.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
-
+import Atistas from './Artist';
 const Principal = () => {
   return (
     <div className='main'>
         <h2>ARTISTAS POPULARES</h2>
         <div className='artistas-populares'>
           {/* Cada artista vira um componente ou card */}
+          <Atistas />
         </div>
         <div>
           <h2>MÚSICAS DO MOMENTO</h2>
