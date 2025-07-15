391aa5a75dde01ca71ab7ba58df5fd04edd41df0
Ariel dos Reis Marinho <arieldreis@gmail.com>
2025-07-15 13:17:52 -0300

    Finishin database Js

--- SpotifyHash/my_spotify/src/assets/artist.js
+++ SpotifyHash/my_spotify/src/assets/artist.js
@@ -27,59 +27,97 @@ import Iza from './Singers/Iza.jpeg';
 import ImagineDragons from './Singers/ImagineDragons.jpeg';
 import HarryStyles from './Singers/HarryStyles.avif';
 import Nathanzinho from './Singers/Nathanzinho.jpeg';
-import GustavoLima from './Singers/';
-const artist = [
+import GustavoLima from './Singers/Gustavolima.jpeg';
+import BrunoMars from './Singers/Bruno_Mars.jpeg';
+import Cent50 from './Singers/50Cent.jpeg';
+import ABBA from './Singers/ABBA.webp';
+import ACDC from './Singers/AcDc.jpeg'
+import Akon from './Singers/Akon.jpg';
+import Anitta from './Singers/Annita.jpeg'
+import ArianaGrande from './Singers/ArianaGrande.webp';
+import BadBunny from './Singers/BadBunny.avif';
+import Beyonce from './Singers/Beyonce.webp';
+import BillieAlish from './Singers/BilleiAllih.gif';
+import BriteneySpears from './Singers/BritneySpears.webp'
+import CardiB from './Singers/CardiB.jpeg';
+import ColdPlay from './Singers/ColdPlay.webp';
+import CrisBrown from './Singers/CrisBrown.jpg';
+import Djavan from './Singers/Djavan.jpeg';
+import DojaCat from './Singers/DojaCat.jpeg';
+import DrDre from './Singers/DrDre.jpeg';
+import Drake from './Singers/Drake.jpeg';
+import DuaLipa from './Singers/DuaSpotify.jpeg';
+import EdSherren from './Singers/Ed_Sheeran.jpg';
+import EltonJohn from './Singers/EltonJohn.jpg';
+import Emineen from './Singers/Eminne.webp';
+import HenriqueJuliano from './Singers/Henrique%26Juliano.jpeg';
+import LuanSantana from './Singers/LuanSantana.jpg';
+import MatheusKauan from './Singers/Matheus%26Kauan.jpg';
+import McTuto from './Singers/McTuto.avif';
+import Marron5 from './Singers/Marron5.jpeg';
+import Rihanna from './Singers/Rihanna.jpeg';
+import LuisFonsi from './Singers/luizFonsi.jpg';
+import Blackpink from './Singers/BlackPink.jpeg';
 
-    {nome: "The Weekend", img: theWeekend},
-    {nome: "Travis Scott", img: TravisScoot.webp},
-    {nome: "The Box Medley", img: TheBoxMadely},
-    {nome: "Taylor Swift", img: TaylorSwift},
-    {nome: "SZA", img: ""},
-    {nome: "", img: ""},
-    {nome: "", img: ""},
-    {nome: "", img: ""},
-    {nome: "", img: ""},
-    {nome: "", img: ""},
-    {nome: "", img: ""},
-    {nome: "", img: ""},
-    {nome: "", img: ""},
-    {nome: "", img: ""},
-    {nome: "", img: ""},
-    {nome: "", img: ""},
-    {nome: "", img: ""},
-    {nome: "", img: ""},
-    {nome: "", img: ""},
-    {nome: "", img: ""},
-    {nome: "", img: ""},
-    {nome: "", img: ""},
-    {nome: "", img: ""},
-    {nome: "", img: ""},
-    {nome: "", img: ""},
-    {nome: "", img: ""},
-    {nome: "", img: ""},
-    {nome: "", img: ""},
-    {nome: "", img: ""},
-    {nome: "", img: ""},
-    {nome: "", img: ""},
-    {nome: "", img: ""},
-    {nome: "", img: ""},
-    {nome: "", img: ""},
-    {nome: "", img: ""},
-    {nome: "", img: ""},
-    {nome: "", img: ""},
-    {nome: "", img: ""},
-    {nome: "", img: ""},
-    {nome: "", img: ""},
-    {nome: "", img: ""},
-    {nome: "", img: ""},
-    {nome: "", img: ""},
-    {nome: "", img: ""},
-    {nome: "", img: ""},
-    {nome: "", img: ""},
-    {nome: "", img: ""},
-    {nome: "", img: ""},
-    {nome: "", img: ""},
-    {nome: "", img: ""},
-    {nome: "", img: ""},
-    {nome: "", img: ""}
+const artist = [
+    { id: 1, nome: "The Weeknd", img: theWeekend },
+    { id: 2, nome: "Travis Scott", img: TravisScoot },
+    { id: 3, nome: "The Box Medley", img: TheBoxMadely },
+    { id: 4, nome: "Taylor Swift", img: TaylorSwift },
+    { id: 5, nome: "SZA", img: SZA },
+    { id: 6, nome: "Shawn Mendes", img: ShawMendes },
+    { id: 7, nome: "Seu Jorge", img: Jorge },
+    { id: 8, nome: "Sean Paul", img: SeanPaul },
+    { id: 9, nome: "Romeo Santos", img: RomeoSantos },
+    { id: 10, nome: "Queen", img: Queen },
+    { id: 11, nome: "Post Malone", img: PostMalone },
+    { id: 12, nome: "Pitbull", img: Pitbull },
+    { id: 13, nome: "OneRepublic", img: OneRepublic },
+    { id: 14, nome: "One Direction", img: OneDirection },
+    { id: 15, nome: "Milionário & José Rico", img: JoseMilionario },
+    { id: 16, nome: "Michael Jackson", img: MichaelJackson },
+    { id: 17, nome: "Menudo", img: menudo },
+    { id: 18, nome: "MC Cabelinho", img: mcCabelinho },
+    { id: 19, nome: "Matuê", img: Matue },
+    { id: 20, nome: "Lionel Richie", img: LionelRichie },
+    { id: 21, nome: "Lana Del Rey", img: LanaDelRey },
+    { id: 22, nome: "Katy Perry", img: KatyPerrry },
+    { id: 23, nome: "Justin Bieber", img: JustinBieber },
+    { id: 24, nome: "Jorge & Mateus", img: JorgeMateus },
+    { id: 25, nome: "J Balvin", img: JBavin },
+    { id: 26, nome: "IZA", img: Iza },
+    { id: 27, nome: "Imagine Dragons", img: ImagineDragons },
+    { id: 28, nome: "Harry Styles", img: HarryStyles },
+    { id: 29, nome: "Nattanzinho", img: Nathanzinho },
+    { id: 30, nome: "Gusttavo Lima", img: GustavoLima },
+    { id: 31, nome: "Bruno Mars", img: BrunoMars },
+    { id: 32, nome: "50 Cent", img: Cent50 },
+    { id: 33, nome: "ABBA", img: ABBA },
+    { id: 34, nome: "AC/DC", img: ACDC },
+    { id: 35, nome: "Akon", img: Akon },
+    { id: 36, nome: "Anitta", img: Anitta },
+    { id: 37, nome: "Ariana Grande", img: ArianaGrande },
+    { id: 38, nome: "Bad Bunny", img: BadBunny },
+    { id: 39, nome: "Beyoncé", img: Beyonce },
+    { id: 40, nome: "Billie Eilish", img: BillieAlish },
+    { id: 41, nome: "Britney Spears", img: BriteneySpears },
+    { id: 42, nome: "Cardi B", img: CardiB },
+    { id: 43, nome: "Coldplay", img: ColdPlay },
+    { id: 44, nome: "Chris Brown", img: CrisBrown },
+    { id: 45, nome: "Djavan", img: Djavan },
+    { id: 46, nome: "Doja Cat", img: DojaCat },
+    { id: 47, nome: "Dr. Dre", img: DrDre },
+    { id: 48, nome: "Drake", img: Drake },
+    { id: 49, nome: "Dua Lipa", img: DuaLipa },
+    { id: 50, nome: "Ed Sheeran", img: EdSherren },
+    { id: 51, nome: "Elton John", img: EltonJohn },
+    { id: 52, nome: "Eminem", img: Emineen },
+    { id: 53, nome: "Henrique & Juliano", img: HenriqueJuliano },
+    { id: 54, nome: "Luan Santana", img: LuanSantana },
+    { id: 55, nome: "Matheus & Kauan", img: MatheusKauan },
+    { id: 56, nome: "MC Tuto", img: McTuto },
+    { id: 57, nome: "Maroon 5", img: Marron5 },
+    { id: 58, nome: "Rihanna", img: Rihanna },
+    { id: 59, nome: "Luis Fonsi", img: LuisFonsi },
+    { id: 60, nome: "BLACKPINK", img: Blackpink }
 ];
