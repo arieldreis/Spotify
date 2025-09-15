befc8ff3ebb93be1342e9498c99a81bdb9047867
Ariel dos Reis Marinho <arieldreis@gmail.com>
2025-09-15 11:34:21 -0300

    Add deployment scripts and update allowed hosts in Vite config

--- SpotifyHash/my_spotify/vite.config.js
+++ SpotifyHash/my_spotify/vite.config.js
@@ -4,11 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:'/',
   server:{
-    // Atenção: Isso permite acesso de qualquer host.
-    // Use apenas para testes de desenvolvimento.
+    
     allowedHosts:[
-      '366ce0483bd3.ngrok-free.app'
+      'eaddaffd3efe.ngrok-free.app'
     ]
   }
 })
